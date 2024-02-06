6596839491e03ed8ca78a9e3ec2437bef1d0cf25
sangjun1126 <jeongsangjun1234@gmail.com>
2024-02-06 17:45:52 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { dummy } from "./movieDummy";
+import Movie from "./components/Movie";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="app-container">
+        {
+          dummy.results.map((item) => {
+            return (
+              <Movie 
+                title={item.title}
+                poster_path={item.poster_path}
+                vote_average={item.vote_average}
+              />
+            )
+          })
+        }
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
