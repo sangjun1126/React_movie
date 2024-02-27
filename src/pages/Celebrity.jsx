fd462b59c514245b538ad467ca69ce7479287418
sangjun1126 <jeongsangjun1234@gmail.com>
2024-02-27 17:18:00 +0900

    중복 코드 제거

--- src/pages/Celebrity.jsx
+++ src/pages/Celebrity.jsx
@@ -3,7 +3,8 @@ import React from 'react'
 const Celebrity = () => {
   return (
     <div>
-      Celebrity
+      <h1>인물 소개하기</h1>
+      <h2>곧 업데이트 예정입니다.</h2>
     </div>
   )
 }
--- src/pages/Tv.jsx
+++ src/pages/Tv.jsx
@@ -3,7 +3,8 @@ import React from 'react'
 const Tv = () => {
   return (
     <div>
-      Tv
+      <h1>Tv 컴포넌트</h1>
+      <h2>여기 역시 내일 업데이트되어요</h2>
     </div>
   )
 }
